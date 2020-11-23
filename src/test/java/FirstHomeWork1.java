da42d68c7b8fb49ed6deb8f845fa5c2151c54c59
Deniss <deniss1@inbox.ru>
2020-11-23 13:12:14 +0200

    Pervaja domashka, 1 zadanie

--- src/test/java/FirstHomeWork1.java
+++ src/test/java/FirstHomeWork1.java
@@ -1,21 +1,26 @@
 import org.junit.jupiter.api.Test;
 
 public class FirstHomeWork1 {
+    public Integer a  = 100000;
+    public Integer b = 30;
+    public double c = 2.08;
 
     @Test
     public void finalSummOfLoan() {
 
-        System.out.println("Main sum is : " + 100000);
+        System.out.println("Main sum is : " + a );
+        System.out.println("Sum of percent is: " + summOfPercent(a, b, c));
+        double summ = summOfPercent(a, b, c);
+        System.out.println("Final sum of loan is: " + finalLoanAmout(summ, a));
 
-        System.out.println("Sum of percent is: " + summOfPercent(100000,30, 2.09));
-
-        System.out.println("Final sum of loan is: " + finalLoanAmout(62700, 100000));
     }
+
     public double summOfPercent(Integer a, Integer b, double c) {
         double d = a * c/100 * b;
         return d;
 
     }
+
     public int finalLoanAmout (double d, Integer a) {
         double e = d + a;
         return (int) e;
