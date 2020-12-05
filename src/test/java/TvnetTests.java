414eded6a13ec98f27e263f94cccb676094323e4
Deniss <deniss1@inbox.ru>
2020-12-05 16:56:50 +0200

    TVnet zahod na stranicu, na ljubuju statjju i potom na komenti. Proba 2 zadanija, ne dodelano...

--- src/test/java/TvnetTests.java
+++ src/test/java/TvnetTests.java
@@ -1,13 +1,17 @@
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+
 public class TvnetTests {
     private final By ACCEPT_COOKIES_BTN = By.xpath(".//button");
+    private final By LIST_OF_HEADLINES = By.xpath(".//*[@class = 'list-article__headline']");
 
 
     @Test
@@ -28,4 +32,23 @@ public class TvnetTests {
 
 
     }
+    @Test
+    public void secondTest() {
+        System.setProperty("webdriver.chrome.driver", "C://chromedriver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://tvnet.lv");
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(ACCEPT_COOKIES_BTN));
+
+        WebElement acceptBtn = driver.findElement(ACCEPT_COOKIES_BTN);
+        acceptBtn.click();
+
+        List listOfHeadlines = driver.findElements(LIST_OF_HEADLINES);
+        System.out.println(listOfHeadlines);
+
+    }
 }
+
+
